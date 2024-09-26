f044179db6423d6538c50d3daf11130a80d1f06b
HouckDev <houckdeveloper@gmail.com>
2024-09-25 23:12:28 -0500

    Part 11.8; Implement NewBlock.eqv

--- src/main/java/edu/grinnell/csc207/blocks/PatternBorder.java
+++ src/main/java/edu/grinnell/csc207/blocks/PatternBorder.java
@@ -91,11 +91,26 @@ public class PatternBorder implements AsciiBlock {
   /**
    * Determine if another block is structurally equivalent to this block.
    *
-   * @param other The block to compare to this block.
+   * @param other
+   *   The block to compare to this block.
    *
-   * @return true if the two blocks are structurally equivalent and false otherwise.
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
    */
   public boolean eqv(AsciiBlock other) {
-    return false; // STUB
+    return ((other instanceof PatternBorder) && (this.eqv((PatternBorder) other)));
   } // eqv(AsciiBlock)
+
+  /**
+   * Determine if another VFlip is structurally equivalent to this block.
+   *
+   * @param other
+   *   The block to compare to this block.
+   *
+   * @return true if the two blocks are structurally equivalent and
+   *     false otherwise.
+   */
+  public boolean eqv(PatternBorder other) {
+    return this.pattern.equals(other.pattern) && this.contents.eqv(other.contents);
+  } // eqv(VFlip)
 } // class Surrounded
